19f6313d7b34753005e5769b2a08b9b828002e55
synnea <carrie.poell@gmail.com>
2021-01-14 09:31:49 +0100

    create functioning toggle button

---
+++ src/components/Navigation/Sidedrawer/ToggleButton/ToggleButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const toggleButton = (props) => (
+  <p onClick={props.clicked}>MENU</p>
+);
+
+export default toggleButton;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import ToggleButton from '../Sidedrawer/ToggleButton/ToggleButton';
+
+
+const toolbar = (props) => {
+    return(
+    <header className={classes.Toolbar}>
+        <ToggleButton clicked={props.toggleButtonClicked} />
+        <div className={classes.Logo}>
+            <Logo /> 
+        </div> 
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+    </header>
+    )
+};
+
+export default toolbar;
